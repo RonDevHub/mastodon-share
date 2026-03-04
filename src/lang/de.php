c6e5d082b7fe29bd3c13d74be0fef14db4063645
RonDevHub <no_reply@rondev.de>
2026-03-04 10:50:34 +0000

    src/lang/de.php hinzugefügt

---
+++ src/lang/de.php
@@ -0,0 +1,13 @@
+<?php
+// de.php
+return [
+    'title' => 'Auf Mastodon teilen',
+    'instance_label' => 'Deine Instanz (z.B. mastodon.social)',
+    'share_button' => 'Jetzt Tröten!',
+    'forget' => 'Instanz vergessen',
+    'preview_title' => 'Vorschau Deines Beitrags',
+    'captcha_label' => 'Sicherheit: Was ist %d + %d?',
+    'error_invalid' => 'Das scheint keine gültige Mastodon-Instanz zu sein.',
+    'generator_title' => 'Button Generator',
+    'copy_link' => 'Link kopieren'
+];
