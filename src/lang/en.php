260e36f08cf06192d95b7e029ca355a50b1dc6a9
RonDevHub <no_reply@rondev.de>
2026-03-04 10:52:00 +0000

    src/lang/en.php hinzugefügt

---
+++ src/lang/en.php
@@ -0,0 +1,13 @@
+<?php
+// en.php
+return [
+    'title' => 'Share on Mastodon',
+    'instance_label' => 'Your instance (e.g. mastodon.social)',
+    'share_button' => 'Toot now!',
+    'forget' => 'Forget instance',
+    'preview_title' => 'Preview of your post',
+    'captcha_label' => 'Security: What is %d + %d?',
+    'error_invalid' => 'That doesn’t seem to be a valid Mastodon instance.',
+    'generator_title' => 'Button Generator',
+    'copy_link' => 'Copy link'
+];
