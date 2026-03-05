b64dd717ddfe69784b7c39355cd68b820ae34197
RonDevHub <no_reply@rondev.de>
2026-03-05 07:54:48 +0000

    src/lang/de.php aktualisiert

--- src/lang/de.php
+++ src/lang/de.php
@@ -1,13 +1,16 @@
 <?php
 return [
-    'title' => '🐘Mastodon Share',
+    'title' => 'Mastodon Share',
     'placeholder' => 'deine-instanz.de',
+    'your_instance' => 'Deine Instanz',
     'btn_share' => 'Jetzt Tröten',
     'captcha_q' => 'Sicherheit: %d + %d = ?',
     'err_instance' => 'Ungültige Instanz!',
     'err_captcha' => 'Mathe-Check fehlgeschlagen.',
     'forget' => 'Instanz vergessen',
     'gen_title' => 'Generator',
+    'gen_open' => 'Generator öffnen',
     'preview' => 'Beitrag bearbeiten',
-    'copy_success' => 'Kopiert! 📋'
+    'copy_success' => 'Kopiert! 📋',
+    'created_by' => 'Erstellt mit ❤️ und ☕️ von'
 ];
